dd5af303d79ce6d67cb040ea0b6408428bc063b7
jorgeboucas <mail@jorgeboucas.com>
2019-10-02 12:08:31 +0200

    DScapstone

---
+++ DScapstone.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_markers: '{{{,}}}'
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# ### Intro
+#
+# This is my first notebook for Courseras Capstone project on DS.
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
